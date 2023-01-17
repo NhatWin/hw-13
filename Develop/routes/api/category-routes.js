301cfe651bd3f465849b42dc354de09c73101b7d
NhatWin <nhat0933@gmail.com>
2023-01-17 13:07:49 -0600

    Download node packages

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,26 +3,50 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', (_, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [Product]
+  }).then(categorysData => {
+    res.json(categorysData)
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    include: [Product],
+    where: {
+      id: req.params.id
+    }
+  }).then(data => {
+    res.json(data)
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new category
-});
+  Category.create(req.body)
+}).then(res.json("Category Saved"));
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-});
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+}).then(res.json("Category Updated"));
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destory({
+    where: {
+      id: req.params.id
+    }
+  })
 });
 
 module.exports = router;
